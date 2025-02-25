d224cadd4811bd15d9ae74bfda9effc83a85b3a9
MateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-02-25 11:22:32 -0500

    feat: base proyect

---
+++ src/test/java/co/edu/eci/pigball/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eci.pigball.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
